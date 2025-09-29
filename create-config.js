f22f838e0c648deb78c1ce470184c5cb24900a6b
sooeeren <sooeeren@proton.me>
2025-09-29 20:02:10 +0200

    add: debug

--- create-config.js
+++ create-config.js
@@ -1,7 +1,16 @@
-// This is a Node.js script that runs on Vercel's servers during deployment.
 const fs = require('fs');
 
-// It reads the secret Environment Variables...
+// --- DEBUGGING: Print the environment variables Vercel sees ---
+console.log('--- Vercel Environment Variable Check ---');
+console.log('API_KEY:', process.env.API_KEY ? 'Loaded' : '!!! UNDEFINED !!!');
+console.log('AUTH_DOMAIN:', process.env.AUTH_DOMAIN ? 'Loaded' : '!!! UNDEFINED !!!');
+console.log('PROJECT_ID:', process.env.PROJECT_ID ? 'Loaded' : '!!! UNDEFINED !!!');
+console.log('STORAGE_BUCKET:', process.env.STORAGE_BUCKET ? 'Loaded' : '!!! UNDEFINED !!!');
+console.log('MESSAGING_SENDER_ID:', process.env.MESSAGING_SENDER_ID ? 'Loaded' : '!!! UNDEFINED !!!');
+console.log('APP_ID:', process.env.APP_ID ? 'Loaded' : '!!! UNDEFINED !!!');
+console.log('------------------------------------');
+// --- END DEBUGGING ---
+
 const config = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
@@ -11,10 +20,6 @@ const config = {
   appId: process.env.APP_ID,
 };
 
-// ...and writes them into the firebase-config.js file with the correct module syntax.
-// The 'export default' is included here.
 const configFileContent = `export default ${JSON.stringify(config, null, 2)};`;
-
 fs.writeFileSync('firebase-config.js', configFileContent);
-
 console.log('Successfully created firebase-config.js');
